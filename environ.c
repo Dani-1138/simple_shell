f86a53fe74e31f0d26939d74ed836d75c1018b13
Dani-1138 <danieldemeke92@gmail.com>
2023-02-21 12:02:43 -0800

    simple shell

--- environ.c
+++ environ.c
@@ -2,10 +2,10 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0 
+ * @info: Structure containing potential arguments.
+ * Return: Always 0
  */
+
 int _myenv(info_t *info)
 {
 	print_list_str(info->env);
--- getenv.c
+++ getenv.c
@@ -1,9 +1,8 @@
 #include "shell.h"
 
 /**
- * get_environ - returns the string array copy of our environ 
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * get_environ - returns the string array copy of our environ
+ * @info: Structure containing potential arguments.
  * Return: Always 0
  */
 char **get_environ(info_t *info)
